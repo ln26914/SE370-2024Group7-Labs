015f20084fd4fb05e9b5b59f8ee7b1af49b30246
Noah Boyd <121831326+ln26914@users.noreply.github.com>
2024-09-20 16:52:57 -0700

    Added main.java comment and class framework

--- Lab4/main.java
+++ Lab4/main.java
@@ -2,3 +2,47 @@
 Lab4: Render playing card images in a 4x13 grid, with a button to shuffle.
 
 */
+import javax.swing.*;
+
+public class CardShufflerUI {
+    // Define Class Variables
+    
+    // Define Class Functions
+
+    /**
+     * CardShufflerUI()
+     * Usage: Constructor Function for objects of type CardShufflerUI
+     * Given no parameters, create a new CardShufflerUI
+     * Returns Nothing
+     * Usage: sampleDeckUI = new CardShufflerUI()
+     */
+    public CardShufflerUI() {
+        
+        // Create the UI Window
+    }
+    
+    /**
+     * shuffle()
+     * Usage: Randomize the order of cards within a deck of cards.
+     * Given no parameters, randomize the caller's card order.
+     * Returns Nothing.
+     * Usage: sampleDeck.shuffle() // Shuffles sampleDeck, changing its order.
+     */
+    public void shuffle() {
+
+        // Randomize the order of the cards within the deck.
+        // Card Deck Data Type: #### AS DEFINED ABOVE ####
+
+    }
+
+    /** 
+     * main()
+     * Usage: Test the functionality of the CardShufflerUI class.
+     * Given no parameters, produce the window object
+     * Returns Nothing
+     * Usage: main()
+    */
+    public static void main(String[] args) {
+        // Main Function
+    }
+}
